8e893916590369f0b0a30ad84661e24e7bd3d6fb
katyjane8 <katy.jane8@gmail.com>
2017-12-10 10:45:51 -0700

    comment function added to all pages - renders to show page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
-class Article < ApplicationRecord
+class Article < ActiveRecord::Base
+  #don't totally understand why we had to change this from ApplicationRecord to ActiveRecord::Base
+  has_many :comments
+
 end
